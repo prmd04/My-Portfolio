f504b7a173886d9bf74060629de7fdb79f3d42fb
pramod ade <prmdextracthere@gmail.com>
2026-03-18 08:34:20 +0530

    changes in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -64,7 +64,7 @@ const Footer = () => {
 
         {/* Copyright Text */}
         <p className="text-sm text-gray-400 mt-6">
-          © 2025 Pramod Ade. All rights reserved.
+          © 2026 Pramod Ade. All rights reserved.
         </p>
       </div>
     </footer>
